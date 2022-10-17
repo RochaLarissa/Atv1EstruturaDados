3d756a54636133cb449c93d5bccb178cc8e8e73d
lapc <larissa.rocha@gft.com>
2022-10-17 16:23:01 -0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		 
+		int opcao;
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("-------------------");
+		System.out.println("|  1 - Vetor      |");
+		System.out.println("|  2 - Lista      |");
+		System.out.println("-------------------");
+		System.out.println("\nSelecione a opção desejada:");
+
+		opcao = s.nextInt();
+		
+		switch (opcao) {
+		case 1: {
+			System.out.println("---------------------");
+			System.out.println("|        Vetor       |");
+			System.out.println("---------------------");
+			System.out.println("| 1 - Inserir dado   |");
+			System.out.println("| 2 - Alterar dado   |");
+			System.out.println("| 3 - Pesquisar dado |");
+			System.out.println("| 4 - Mostrar dados  |");
+			System.out.println("| 5 - Voltar         |");
+			System.out.println("\nSelecione a opção desejada:");
+		};
+		case 2: {
+			System.out.println("---------------------");
+			System.out.println("|        Lista       |");
+			System.out.println("---------------------");
+			System.out.println("| 1 - Inserir dado   |");
+			System.out.println("| 2 - Alterar dado   |");
+			System.out.println("| 3 - Excluir dado   |");
+			System.out.println("| 4 - Pesquisar dado |");
+			System.out.println("| 5 - Mostrar dados  |");
+			System.out.println("| 6 - Voltar         |");
+			System.out.println("\nSelecione a opção desejada:");
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + opcao);
+		}
+		
+		
+		
+	}
+
+}
